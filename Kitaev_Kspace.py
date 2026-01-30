c711ec2110c45f76760217fd266e14a0fa063688
haneefquanta <www.haneef777@gmail.com>
2026-01-30 10:52:46 +0530

    mu title

--- Kitaev_Kspace.py
+++ Kitaev_Kspace.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 N_k = 400         # system size (large for bulk)
 t = 1             # hopping
 Delta = 1         # pairing
-mu = 2   #E±​(k)=±(ε(k)**2+Δ(k)**2)**0.5
+mu = 1.5 
 
 def Hk(eps, delta_k):   
         return np.array([[eps, 1j*delta_k],
@@ -14,16 +14,16 @@ def Hk(eps, delta_k):
 
 
 def kitaev_k_space(mu, t, Delta, N_k): 
-    ks = 2*np.pi*np.arange(N_k)/N_k # k = 2*p*n/N_k (N_K is the resolution) 
+    ks = np.linspace(-np.pi, np.pi, N_k, endpoint=False) # k = 2*p*n/N_k (N_K is the resolution) 
     energies = np.zeros((N_k,2))    #initiating the energies 
     for i,k in enumerate(ks):       
-        eps = -mu - t*np.cos(k)  # eps = -tcosk-mu 
-        delta_k = Delta*np.sin(k) #delta_k = del * sin(k)
+        eps = -mu - 2*t*np.cos(k)  # eps = -2tcosk-mu 
+        delta_k = 2*Delta*np.sin(k) #delta_k = 2del * sin(k)
         vals, vecs = np.linalg.eigh(Hk(eps, delta_k))  # Diagonalisation
-        energies[i,:] = np.sort(vals) # each row contain +E and -E 
+        energies[i,:] = np.sort(vals) # each row contain +E and -E  ,#E±​(k)=±(ε(k)**2+Δ(k)**2)**0.5
     return ks, energies
 
- 
+
 ks, E = kitaev_k_space(mu,t, Delta, N_k) 
 
 plt.figure(figsize=(6,4))
@@ -33,7 +33,8 @@ plt.axhline(0, color='k', linewidth=0.5)
 
 plt.xlabel(r'$k$')
 plt.ylabel(r'Energy $E(k)$')
-plt.title('Kitaev Chain BdG Spectrum (k-space)')
+plt.title(f'Kitaev Chain BdG Spectrum (k-space), mu = {mu}')
+
 plt.legend()
 plt.tight_layout()
 plt.show()
